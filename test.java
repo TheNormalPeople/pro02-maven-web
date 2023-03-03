b7d3b305ccca8249148abb9b3b3747d437c68429
cyhien <1272481411@qq.com>
2023-03-03 16:30:00 +0800

    no_1

---
+++ test.java
@@ -0,0 +1,13 @@
+/**
+*@Author: chenyi
+*@CreateTime: 2023-03-03  16:28
+*@BelongsProject: pro02-maven-web
+*@BelongsPackage: PACKAGE_NAME
+*@Version: 1.0
+*@Description: TODO
+*/
+public class test {
+    public static void main(String[] args) {
+        System.out.println("aaaaa");
+    }
+}
